df0ac379641d45c01cb97b4e19a6d4f4c364ff8e
Yohan Rizk <yrizk@yrizk.roam.corp.google.com>
2018-09-20 13:51:40 -0400

    got the triangle to show without proper projection

---
+++ app/src/main/java/com/example/yrizk/ultrasound/UltrasoundRenderer.java
@@ -0,0 +1,55 @@
+package com.example.yrizk.ultrasound;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public  class UltrasoundRenderer implements GLSurfaceView.Renderer {
+
+    private static final String TAG = "UltrasoundRenderer";
+
+    private float[] mvpMatrix = new float[16];
+    private float[] viewMatrix = new float[16];
+    private float[] projectionMatrix  = new float[16];
+
+    private Triangle triangle;
+    private Square square;
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        triangle = new Triangle();
+        square = new Square();
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        // Draw shape
+        triangle.draw();
+    }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f);
+    }
+
+    public static int loadShader(int type, String shaderCode) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+        return shader; // the reference.
+    }
+}
